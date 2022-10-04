f78e6ea5b8c5bf8bee807b43b6130c7aca6b13a1
ingridFacens <ingrid.camargo_@hotmail.com>
2022-10-03 21:28:22 -0300

    Add header and footer content

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,17 @@ import React from 'react';
 import style from './Footer.module.css';
 
 const Footer = () => {
-  return <div></div>;
+  return (
+    <footer>
+      {/* <h2>Descubra lugares para descartar eletrônicos e eletrodomésticos próximo da sua região</h2>
+
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.</p>
+    
+      <button type="button">Buscar por locais</button> */}
+
+      <img src="" alt="" />
+    </footer>
+  )
 };
 
 export default Footer;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,29 @@ import React from 'react';
 import style from './Header.module.css';
 
 const Header = () => {
-  return <div></div>;
+  return (
+    <header>
+      <nav>
+        <a href="/">
+          <img src="" alt="" />
+        </a>
+
+        {/* <a href="#">
+          <span></span>
+          <span></span>
+          <span></span>
+        </a> */}
+
+        <ul> 
+          <li><a href="#">Sobre o projeto</a></li>
+          <li><a href="#">O que são lixos eletrônicos</a></li>
+          <li><a href="#">Contato</a></li>
+          <li><a href="#">Cadastre-se</a></li>
+        </ul>
+      
+      </nav>
+    </header>
+  )
 };
 
 export default Header;
