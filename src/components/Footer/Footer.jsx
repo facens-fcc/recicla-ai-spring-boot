2ed3a5bbe6e3d01ecb694ca0700b0b4df171c5f2
Ingrid Camargo <ingrid.camargopr@gmail.com>
2022-10-06 16:01:49 -0300

    fix footer logo alignment

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,14 +3,9 @@ import style from './Footer.module.css';
 
 const Footer = () => {
   return (
-    <footer>
-      {/* <h2>Descubra lugares para descartar eletrônicos e eletrodomésticos próximo da sua região</h2>
-
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.</p>
-    
-      <button type="button">Buscar por locais</button> */}
-
-      <img src="" alt="" />
+    <footer className={style.footer}>
+      {/* <img src="" alt="" /> */}
+      <a href="#">Recicla.aí</a>
     </footer>
   )
 };
