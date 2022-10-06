805e77163a554997302900e105be474d0cd7b5e6
Ingrid Camargo <ingrid.camargopr@gmail.com>
2022-10-06 16:02:38 -0300

    add mobile menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,25 +1,36 @@
 import React from 'react';
+import { useState } from 'react';
+
 import style from './Header.module.css';
+import classNames from 'classnames';
 
 const Header = () => {
+
+  const [isNavbarOpen, setIsNavbarOpen] = useState(false);
+
+  function handleToggle() {
+    setIsNavbarOpen(!isNavbarOpen);
+  }
+
   return (
-    <header>
-      <nav>
-        <a href="/">
-          <img src="" alt="" />
+    <header className={style.header}>
+      <nav className={style.nav}>
+        <a href="/" className={style.logo}>
+          {/* <img src="" alt="" /> */}
+          Recicla.aí
         </a>
 
-        {/* <a href="#">
+        <a href="#" className={isNavbarOpen ? style.headerToggle__open : style.headerToggle} onClick={handleToggle}>          
           <span></span>
           <span></span>
           <span></span>
-        </a> */}
+        </a>
 
-        <ul> 
-          <li><a href="#">Sobre o projeto</a></li>
-          <li><a href="#">O que são lixos eletrônicos</a></li>
-          <li><a href="#">Contato</a></li>
-          <li><a href="#">Cadastre-se</a></li>
+        <ul className={isNavbarOpen ? style.menuOpen : style.menu}> 
+          <li className={style.menuItem}><a href="#">Sobre o projeto</a></li>
+          <li className={style.menuItem}><a href="#">O que são lixos eletrônicos</a></li>
+          <li className={style.menuItem}><a href="#">Contato</a></li>
+          <li className={style.menuItem}><a href="#" className={style.menuItem__green}>Cadastre-se</a></li>
         </ul>
       
       </nav>
