6b6d30cfdf84637cd05fba168da616a8e4493296
Matheus Carvalho <henriquematheus.c@gmail.com>
2022-10-05 21:44:26 -0300

    Create Typography components

--- src/components/Heading/Heading.jsx
+++ src/components/Heading/Heading.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import style from './Heading.module.css';
 
-const Heading = () => {
-  return <div></div>;
+const Heading = ({ level, size, align, children }) => {
+  const HeadingTag = level;
+
+  const sizeClass = () => {
+    switch (size) {
+      case 'lg':
+        return style.large;
+      case 'sm':
+        return style.small;
+      case 'md':
+        return style.medium;
+      default:
+        return 'md';
+    }
+  };
+
+  return <HeadingTag className={`${style.heading} ${sizeClass()}`}>{children}</HeadingTag>;
 };
 
 export default Heading;
--- src/components/Overtitle/Overtitle.jsx
+++ src/components/Overtitle/Overtitle.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import style from './Overtitle.module.css';
 
-const Overtitle = () => {
-  return <div></div>;
+const Overtitle = ({ level, align, children }) => {
+  const OvertitleTag = level;
+
+  const alignmentClass = () => {
+    switch (align) {
+      case 'center':
+        return style.center;
+      case 'left':
+        return style.left;
+      default:
+        return style.left;
+    }
+  };
+
+  return <OvertitleTag className={`${style.overtitle} ${alignmentClass()}`}>{children}</OvertitleTag>;
 };
 
 export default Overtitle;
