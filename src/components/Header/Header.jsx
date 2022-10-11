19638b1080a5d8e53b24946ccd3cb6011b5f8058
Ingrid Camargo <ingrid.camargopr@gmail.com>
2022-10-11 20:11:40 -0300

    Close menu when resize viewport

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,32 +1,40 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import style from './Header.module.css';
-import classNames from 'classnames';
 
 const Header = () => {
 
   const [isNavbarOpen, setIsNavbarOpen] = useState(false);
 
+  useEffect(() => {
+    window.addEventListener('resize', () => {
+      if(window.matchMedia("(min-width: 768px)").matches) {
+        setIsNavbarOpen(false);
+      } 
+    })
+  }, [setIsNavbarOpen])
+
   function handleToggle() {
     setIsNavbarOpen(!isNavbarOpen);
   }
+
+  
 
   return (
     <header className={style.header}>
       <nav className={style.nav}>
         <a href="/" className={style.logo}>
-          {/* <img src="" alt="" /> */}
           Recicla.aí
         </a>
 
-        <a href="#" className={isNavbarOpen ? style.headerToggle__open : style.headerToggle} onClick={handleToggle}>          
+        <button href="#" className={isNavbarOpen ? `${style.headerToggle} ${style.headerToggle__open}` : `${style.headerToggle}`} onClick={handleToggle}>          
           <span></span>
           <span></span>
           <span></span>
-        </a>
+        </button>
 
-        <ul className={isNavbarOpen ? style.menuOpen : style.menu}> 
+        <ul className={isNavbarOpen ? `${style.menu} ${style.menu__open}` : `${style.menu}`}> 
           <li className={style.menuItem}><a href="#">Sobre o projeto</a></li>
           <li className={style.menuItem}><a href="#">O que são lixos eletrônicos</a></li>
           <li className={style.menuItem}><a href="#">Contato</a></li>
