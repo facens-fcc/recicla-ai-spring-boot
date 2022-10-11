37918934a5310c67a0e921665ddff6660592c55a
Matheus Carvalho <henriquematheus.c@gmail.com>
2022-10-11 20:15:12 -0300

    Remove unused/test code

--- src/components/ResultsFeed/ResultsFeed.jsx
+++ src/components/ResultsFeed/ResultsFeed.jsx
@@ -4,10 +4,6 @@ import companies from '../../data/companies.json';
 import style from './ResultsFeed.module.css';
 
 const ResultsFeed = () => {
-  // Show console mensage when resize and screen is larger than 768px
-
-  window.addEventListener('resize', function(event) {console.log('ta mudando de tamanho');
-
   return (
     <div className={style.resultsFeed}>
       <div className={style.resultsFeed__record}>4 resultados encontrados</div>
