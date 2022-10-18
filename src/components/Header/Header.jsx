511561575ba6d5227ab669aba1910263a3f1e44f
Ingrid Camargo <ingrid.camargopr@gmail.com>
2022-10-17 21:01:21 -0300

    Close menu when resize viewport

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,22 +4,19 @@ import { useState, useEffect } from 'react';
 import style from './Header.module.css';
 
 const Header = () => {
-
   const [isNavbarOpen, setIsNavbarOpen] = useState(false);
 
   useEffect(() => {
     window.addEventListener('resize', () => {
-      if(window.matchMedia("(min-width: 768px)").matches) {
+      if (window.matchMedia('(min-width: 768px)').matches) {
         setIsNavbarOpen(false);
-      } 
-    })
-  }, [setIsNavbarOpen])
+      }
+    });
+  }, [setIsNavbarOpen]);
 
   function handleToggle() {
     setIsNavbarOpen(!isNavbarOpen);
   }
-
-  
 
   return (
     <header className={style.header}>
@@ -28,22 +25,31 @@ const Header = () => {
           Recicla.aí
         </a>
 
-        <button href="#" className={isNavbarOpen ? `${style.headerToggle} ${style.headerToggle__open}` : `${style.headerToggle}`} onClick={handleToggle}>          
+        <button href="#" className={isNavbarOpen ? `${style.headerToggle} ${style.headerToggle__open}` : `${style.headerToggle}`} onClick={handleToggle}>
           <span></span>
           <span></span>
           <span></span>
         </button>
 
-        <ul className={isNavbarOpen ? `${style.menu} ${style.menu__open}` : `${style.menu}`}> 
-          <li className={style.menuItem}><a href="#">Sobre o projeto</a></li>
-          <li className={style.menuItem}><a href="#">O que são lixos eletrônicos</a></li>
-          <li className={style.menuItem}><a href="#">Contato</a></li>
-          <li className={style.menuItem}><a href="#" className={style.menuItem__green}>Cadastre-se</a></li>
+        <ul className={isNavbarOpen ? `${style.menu} ${style.menu__open}` : `${style.menu}`}>
+          <li className={style.menuItem}>
+            <a href="#">Sobre o projeto</a>
+          </li>
+          <li className={style.menuItem}>
+            <a href="#">O que são lixos eletrônicos</a>
+          </li>
+          <li className={style.menuItem}>
+            <a href="#">Contato</a>
+          </li>
+          <li className={style.menuItem}>
+            <a href="#" className={style.menuItem__green}>
+              Cadastre-se
+            </a>
+          </li>
         </ul>
-      
       </nav>
     </header>
-  )
+  );
 };
 
 export default Header;
