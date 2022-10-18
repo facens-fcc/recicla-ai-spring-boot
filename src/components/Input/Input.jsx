fc096cbb3855cd8dbcff14482562e68e7bd683ac
Matheus Carvalho <henriquematheus.c@gmail.com>
2022-10-18 14:17:07 -0300

    Add homepage form

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import style from './Input.module.css';
 
-const Input = () => {
-  return <div></div>;
+const Input = ({ label, id, variant, ...props }) => {
+  return (
+    <div className={style.field}>
+      <label className={style.label} htmlFor={id}>
+        {label}
+      </label>
+      <input className={style.input} id={id} {...props} />
+    </div>
+  );
 };
 
 export default Input;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
+import Input from '../../components/Input/Input';
 
 import CallToAction from '../../components/CallToAction/CallToAction';
 import FeaturedServices from '../../components/FeaturedServices/FeaturedServices.jsx';
@@ -11,7 +12,25 @@ const Home = () => {
     <div>
       <Header />
       <main>
+        <section className="section section--light-grey-color">
+          <div className="container">
+            <div className="stack">
+              <h1 className="heading heading--large">Descubrar lugares para descartar lixos eletrônicos e eletrodométiscos em Sorocaba</h1>
+              <div className="box">
+                <form className="form form--horizontal" action="/resultados" method="GET">
+                  <Input label="O que você deseja descartar?" id="search" name="search" type="text" placeholder="Ex: pilhas, baterias, lâmpadas, etc." required />
+                  <Input label="Onde você está?" id="location" name="location" type="text" placeholder="Ex: Rua São Paulo, 123" required />
+                  <button className="button button--primary" type="submit">
+                    Buscar por locais
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </section>
+
         <FeaturedServices />
+
         <section className="section section--light-grey-color">
           <div className="container">
             <div className="stack">
@@ -35,6 +54,7 @@ const Home = () => {
             </div>
           </div>
         </section>
+
         <CallToAction />
       </main>
       <Footer />
