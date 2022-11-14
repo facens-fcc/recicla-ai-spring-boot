89bef6bbdb50cebd14feb61f8a899bd687b8b9d5
Ingrid Camargo <ingrid.camargopr@gmail.com>
2022-11-14 16:12:38 -0300

    Add and get companies coordinates

--- src/pages/Results/Results.jsx
+++ src/pages/Results/Results.jsx
@@ -20,36 +20,52 @@ const Results = () => {
 
   const [originCoordinates, setOriginCoordinates] = useState([]);
   const [destinationCoordinates, setDestinationCoordinates] = useState([]);
+  const [distance, setDistance] = useState(0);
 
   const queryParams = new URLSearchParams(window.location.search)
   const zipCode = queryParams.get('zipCode');
 
-  const companiesZipCode = Companies.map(company => company.address.zip_code);
-  
-  const getCoordinates = async (value) => {
+  const getOriginCoordinates = async (value) => {
     value.toString().includes("-") ? value.replace(/\D/g, '') : value;
     await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${value}&key=AIzaSyDdE2m_2nAtfQN9CA3emww375xD5CELjiU`)
       .then((res) => res.json())
       .then((data) => {
-          const lat = data.results[0].geometry.location.lat;
-          const lng = data.results[0].geometry.location.lng;
+          const lat = data.results[0].geometry.location.lat.toString();
+          const lng = data.results[0].geometry.location.lng.toString();
           setOriginCoordinates([lat, lng]);
       })
       .catch(err => console.log(err))
   }
 
   useEffect(() => {
-    getCoordinates(zipCode);
+    getOriginCoordinates(zipCode);
   }, [])
 
+  let companyLat = [];
+  let companyLng = [];
+  Companies.map(company => {
+    companyLat.push(company.address.lat);
+    companyLng.push(company.address.lng);
+  })
+
   const getDistanceByCoordinates = async (latOrigin, lngOrigin, latDestination, lngDestination) => {
-    await fetch(`https://maps.googleapis.com/maps/api/distancematrix/json?origins=${latOrigin}%2C${lngOrigin}&destinations=${latDestination}%2C${lngDestination}&key=AIzaSyDdE2m_2nAtfQN9CA3emww375xD5CELjiU`)
+    await fetch(`https://maps.googleapis.com/maps/api/distancematrix/json?origins=${latOrigin}%2C${lngOrigin}&destinations=${latDestination}%2C${lngDestination}&key=AIzaSyDdE2m_2nAtfQN9CA3emww375xD5CELjiU`, {
+      method: "GET",
+      headers: { 
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers": "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers",
+        },
+    })
       .then((res) => res.json())
       .then((data) => {
-          console.log(data);
+        console.log(data)
       })
       .catch(err => console.log(err))
   }
+
+  getDistanceByCoordinates(originCoordinates[0], originCoordinates[1], companyLat[0], companyLng[0]);
 
   return (
     <main>
