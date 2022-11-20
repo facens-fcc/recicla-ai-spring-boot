ebbd865b9eb2edeb30f52931857ba00bdb5291ae
Matheus Carvalho <henriquematheus.c@gmail.com>
2022-11-20 07:49:56 -0300

    Update homepage content and style it

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -20,22 +20,29 @@ const Home = () => {
         </div>
       </section>
 
-      <section className={style.quote}>
-        <figure>
-          <blockquote className={`${style.quote__text} display`} cite="https://www.theworldcounts.com/challenges/waste/electronic-waste-facts">
-            Mais de <span className={style.quote__text__highlight}>80%</span> dos lixos eletrônicos e eletrodométiscos não são reciclados
+      <section className={style.whyRecycle} id="por-que-reciclar">
+        <figure className={style.whyRecycle__quote}>
+          <blockquote className={`${style.whyRecycle__quote__content} display`} cite="https://www.theworldcounts.com/challenges/waste/electronic-waste-facts">
+            Mais de <span className={style.whyRecycle__quote__highlight}>80%</span> dos lixos eletrônicos e eletrodométiscos não são reciclados
           </blockquote>
-          <figcaption>
-            —<cite className={style.quote__source}>World Economic Forum</cite>
+          <figcaption className={style.whyRecycle__quote__source}>
+            —<cite>World Economic Forum</cite>
           </figcaption>
         </figure>
         <h2></h2>
       </section>
 
       <section className={style.worldCount}>
-        <h2 className={`${style.worldCount__title} display`}>Toneladas de lixo eletrônico são jogadas fora no mundo todo</h2>
-        <p className={`${style.worldCount__text} display`}>...sendo mais preciso</p>
-        <p className={`${style.worldCount__number} display display--large`}>44.160.970t</p>
+        <figure className={style.worldCount__quote}>
+          <blockquote className={style.quote__content} cite="https://www.theworldcounts.com/challenges/waste/electronic-waste-facts">
+            <p className={`${style.worldCount__quote__title} display`}>Toneladas de lixo eletrônico são jogadas fora no mundo todo</p>
+            <p className={`${style.worldCount__quote__text} display`}>...sendo mais preciso</p>
+            <p className={`${style.worldCount__quote__number} display display--large`}>50.000.000t</p>
+          </blockquote>
+          <figcaption className={style.worldCount__quote__source}>
+            <cite>— Fonte: The World Counts</cite>
+          </figcaption>
+        </figure>
         <div className={style.worldCount__earth}>
           <img className={style.worldCount__earth__image} src={earth} alt="Imagem do planeta Terra" loading="lazy" />
           <p className={`${style.worldCount__earth__text} display`}>A mudança começa por você</p>
@@ -43,31 +50,31 @@ const Home = () => {
       </section>
 
       <section className={style.benefits}>
-        <div className="container">
+        <div className="container container--narrow">
           <ul className={style.benefits__list}>
             <li className={style.benefits__item}>
               <h2 className="heading">Salve o meio ambiente</h2>
-              <p>Reduzindo a extracão de matéria prima para utilização nas indústrias.</p>
+              <p className={style.benefits__item__description}>Reduzindo a extracão de matéria prima para utilização nas indústrias.</p>
             </li>
             <li className={style.benefits__item}>
               <h2 className="heading">Salve dinheiro</h2>
-              <p>Promovendo a ecônomia</p>
+              <p className={style.benefits__item__description}>Promovendo a ecônomia</p>
             </li>
             <li className={style.benefits__item}>
               <h2 className="heading">Salve espaço</h2>
-              <p>Conservando recursos e energia</p>
+              <p className={style.benefits__item__description}>Conservando recursos e energia</p>
             </li>
             <li className={style.benefits__item}>
               <h2 className="heading">Salve vidas</h2>
-              <p>Evitando a contaminação</p>
+              <p className={style.benefits__item__description}>Evitando a contaminação</p>
             </li>
             <li className={style.benefits__item}>
               <h2 className="heading">Salve o futuro</h2>
-              <p>Preservando o planeta</p>
+              <p className={style.benefits__item__description}>Preservando o planeta</p>
             </li>
             <li className={style.benefits__item}>
               <h2 className="heading">Beneficie a sua comunidade</h2>
-              <p>Contribuindo para o bem-estar</p>
+              <p className={style.benefits__item__description}>Contribuindo para o bem-estar</p>
             </li>
           </ul>
         </div>
