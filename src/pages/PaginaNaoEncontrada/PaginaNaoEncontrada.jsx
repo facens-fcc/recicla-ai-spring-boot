90bc98bedce81702b325d43d4b9c616585f6dfb0
Matheus Carvalho <henriquematheus.c@gmail.com>
2022-11-21 17:05:02 -0300

    Update 404 page content

--- src/pages/PaginaNaoEncontrada/PaginaNaoEncontrada.jsx
+++ src/pages/PaginaNaoEncontrada/PaginaNaoEncontrada.jsx
@@ -6,7 +6,7 @@ const PaginaNaoEncontrada = () => {
     <main className="main">
       <section className={style.hero}>
         <div className="container">
-          <h1 className="display display--medium">Páginas não encontrada</h1>
+          <h1 className="display display--medium">Página não encontrada</h1>
           <p className={style.hero__description}>A página que você está procurando não existe.</p>
           <a className="button button--primary button--center" href="/">
             Voltar para a página inicial
