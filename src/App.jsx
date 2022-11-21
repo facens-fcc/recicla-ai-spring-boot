ef3787cc2d6c8dc9c9caf6005cb1dad2cb9d1139
Matheus Carvalho <henriquematheus.c@gmail.com>
2022-11-21 17:04:24 -0300

    Add 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Header from '../src/components/Header/Header';
 import Footer from '../src/components/Footer/Footer';
 import Home from './pages/Home/Home';
 import OndeDescartar from './pages/OndeDescartar/OndeDescartar';
+import PaginaNaoEncontrada from './pages/PaginaNaoEncontrada/PaginaNaoEncontrada';
 
 const App = () => {
   return (
@@ -13,6 +14,8 @@ const App = () => {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/onde-descartar" element={<OndeDescartar />} />
+        <Route path="/404" component={<PaginaNaoEncontrada />} />
+        <Route path="*" element={<PaginaNaoEncontrada />} />
       </Routes>
       <Footer />
     </>
---
+++ src/pages/PaginaNaoEncontrada/PaginaNaoEncontrada.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import style from './PaginaNaoEncontrada.module.css';
+
+const PaginaNaoEncontrada = () => {
+  return (
+    <main className="main">
+      <section className={style.hero}>
+        <div className="container">
+          <h1 className="display display--medium">Páginas não encontrada</h1>
+          <p className={style.hero__description}>A página que você está procurando não existe.</p>
+          <a className="button button--primary button--center" href="/">
+            Voltar para a página inicial
+          </a>
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default PaginaNaoEncontrada;
